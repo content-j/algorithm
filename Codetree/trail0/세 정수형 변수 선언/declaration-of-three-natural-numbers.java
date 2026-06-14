329a4427d86f7eb60dcaffc93719efb0f3e61377
content_j <content_j@naver.com>
2026-06-14 14:00:00 +0900

    [Passed] 세 정수형 변수 선언 (109ms, 9MB)

---
+++ Codetree/trail0/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code herey
+        int x = 7;
+        int y = 23;
+        int z = 30;
+        System.out.print(String.format("%d + %d = %d", x, y, z));
+    }
+}
