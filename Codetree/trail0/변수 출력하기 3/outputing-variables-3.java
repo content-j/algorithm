42de7ae0b9e424a0adae8d8d2d02b704414ed2b3
content_j <content_j@naver.com>
2026-06-14 13:59:28 +0900

    [Passed] 변수 출력하기 3 (98ms, 9MB)

---
+++ Codetree/trail0/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        char a = '1';
+        char b = '2';
+        char c = 'C';
+        System.out.println(String.format("%s->%s->%s",a, b, c));
+    }
+}
