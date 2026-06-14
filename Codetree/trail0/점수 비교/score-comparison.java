55719e1f10d373457cd928ffe3fe86f2cbcc6365
content_j <content_j@naver.com>
2026-06-14 14:24:39 +0900

    [Passed] 점수 비교 (127ms, 10MB)

---
+++ Codetree/trail0/점수 비교/score-comparison.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+          // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int mathA = sc.nextInt();
+        int engA = sc.nextInt();
+        int mathB = sc.nextInt();
+        int engB = sc.nextInt();
+        sc.close();
+        
+        if(mathA > mathB && engA > engB){
+            System.out.print(1);
+        }else{
+            System.out.print(0);
+        }
+    }
+}
