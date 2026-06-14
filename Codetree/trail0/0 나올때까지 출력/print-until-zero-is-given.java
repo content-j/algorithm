2f3c9ae9c221eda02768f2193a8123a10cec5b77
content_j <content_j@naver.com>
2026-06-14 14:51:48 +0900

    [Passed] 0 나올때까지 출력 (124ms, 10MB)

---
+++ Codetree/trail0/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            int n = sc.nextInt();
+
+            if(n == 0){
+                sc.close();
+                break;
+            }
+            System.out.println(n);
+        }
+
+    }
+}
