d8a373082b885d299bf2337fbba165d5367cb2c4
content_j <content_j@naver.com>
2026-06-14 14:00:24 +0900

    [Passed] 달에서 무게 구하기 (95ms, 9MB)

---
+++ Codetree/trail0/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int g = 13;
+        double m = 0.165;
+
+        System.out.print(String.format("%d * %f = %.6f", g, m, ((double) g * m )));
+
+    }
+}
