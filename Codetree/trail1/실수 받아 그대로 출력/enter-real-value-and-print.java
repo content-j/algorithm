a86506104c8bfa17c936de76e9240aad2039b5af
content_j <content_j@naver.com>
2026-06-26 17:18:23 +0900

    [Passed] 실수 받아 그대로 출력 (149ms, 12MB)

---
+++ Codetree/trail1/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+        sc.close();
+        System.out.println(String.format("%.2f", n));
+    }
+}
